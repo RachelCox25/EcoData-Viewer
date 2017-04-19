ccc51b1e562bd6ea5faa26db5dc9500edcc69975
RachelCox25 <rruef95@gmail.com>
2017-04-18 20:36:34 -0600

    PROGRESS! Still working on dynamically showing a dumby chart when bird survey isn't selected, but have some checkboxes appearring

--- server.R
+++ server.R
@@ -19,93 +19,170 @@ library(dplyr)
 #load datasets from ggplot2 assignment that uses dataset from EcoData retreiver
 
 
-#occupancyData <- read_tsv("Occupancy_Data.txt")
-#locationData <- read_tsv("Location_Data.txt")
+occupancyData <- read_tsv("Occupancy_Data.txt")
+locationData <- read_tsv("Location_Data.txt")
 
 #portal <- rdataretriever::fetch("portal")
 
-#shinyServer(function(input, output) {
- # output$graph <- renderPlot({
-    
-    #check to see what the graph is
-  #  if(input$selectData == "Bird Survey"){
-  #    if(input$graphType == "Histogram"){
-        
-        #Got stuff working and then it stopped working
-        #hist(bird_size$bird_size_species[,2])
-        #hist(birdSurvey)
-        
-        #just to show a histogram will appear with the right options chosen
-        #hist(AirPassengers)
-        
-  #      ggplot(locationData, aes(PredominantSpeciesType)) + geom_bar(fill=c("red", "blue")) + xlab("Predominant Species Type: Core or Transient") + ylab("Species Type Count")
-      
-  #    }else if(input$graphType == "Scatter Plot"){
-  #      ggplot(locationData, aes(NumSpecies,NDVI)) + geom_point(aes(colour = factor(PredominantSpeciesType), size = Elevation)) + xlab("Total Number of Species")
-          
-        
-  #    }else{
-  #      print("Graph unavailable for this dataset")
-          
-  #      }
-     
-  #  }
-    
-    
-#  })
+shinyServer(function(input, output) {
   
-#}
-#)
-
-function(input, output) {
+  #IF THE DATA INPUT IS BIRD SURVEY, THE GRAPH OUTPUT IS DIFFERENT aka this is hard coded
+  #This can be thought of as a function, needs to get called somewhere
+  birdSurveySelected <- reactive({
+    if(input$selectData == "Bird Survey"){
+      output$graph <- renderPlot({
+        
+        if(input$graphType == "Histogram"){
+          ggplot(locationData, aes(PredominantSpeciesType)) + geom_bar(fill=c("red", "blue")) + xlab("Predominant Species Type: Core or Transient") + ylab("Species Type Count")
+          
+        }else if(input$graphType == "Scatter Plot"){
+          ggplot(locationData, aes(NumSpecies,NDVI)) + geom_point(aes(colour = factor(PredominantSpeciesType), size = Elevation)) + xlab("Total Number of Species")
+        }
+        
+      })}
+  })
   
+  
+  #IF THE DATA INPUT IS NOT BIRD SURVEY, GRAPH OUTPUT IS DYNAMIC WITH DYNAMIC UI aka this is not hard coded
   output$ui <- renderUI({
-    if (is.null(input$input_type))
-      return()
+    if(input$selectData != "Bird Survey"){
+      
+      switch(input$graphType,
+             "Histogram" = checkboxGroupInput(inputId = "histogramVariableOptions", label = "Select a variable to graph",
+                                              choices = c(10, 344, 567)
+             ),
+             "Scatter Plot" = checkboxGroupInput(inputId = "scatplotVariableOptions", label = "Select two variables to graph",
+                                                 choices = c(10, 344, 567)
+             ),
+             "Box-Whisker" = checkboxGroupInput(inputId = "boxVariableOptions1", label = "Select one below",
+                                                choices = c(10, 344, 567)
+             )
+      )
+    }
+  })
+  
+  output$graph <- renderPlot({
     
-    # Depending on input$input_type, we'll generate a different
-    # UI component and send it to the client.
-    switch(input$input_type,
-           "slider" = sliderInput("dynamic", "Dynamic",
-                                  min = 1, max = 20, value = 10),
-           "text" = textInput("dynamic", "Dynamic",
-                              value = "starting value"),
-           "numeric" =  numericInput("dynamic", "Dynamic",
-                                     value = 12),
-           "checkbox" = checkboxInput("dynamic", "Dynamic",
-                                      value = TRUE),
-           "checkboxGroup" = checkboxGroupInput("dynamic", "Dynamic",
-                                                choices = c("Option 1" = "option1",
-                                                            "Option 2" = "option2"),
-                                                selected = "option2"
-           ),
-           "radioButtons" = radioButtons("dynamic", "Dynamic",
-                                         choices = c("Option 1" = "option1",
-                                                     "Option 2" = "option2"),
-                                         selected = "option2"
-           ),
-           "selectInput" = selectInput("dynamic", "Dynamic",
-                                       choices = c("Option 1" = "option1",
-                                                   "Option 2" = "option2"),
-                                       selected = "option2"
-           ),
-           "selectInput (multi)" = selectInput("dynamic", "Dynamic",
-                                               choices = c("Option 1" = "option1",
-                                                           "Option 2" = "option2"),
-                                               selected = c("option1", "option2"),
-                                               multiple = TRUE
-           ),
-           "date" = dateInput("dynamic", "Dynamic"),
-           "daterange" = dateRangeInput("dynamic", "Dynamic")
-    )
+    birdSurveySelected()
+    
+    #just get a graph to show up
+    hist(AirPassengers)
+    
   })
   
-  output$input_type_text <- renderText({
-    input$input_type
-  })
   
-  output$dynamic_value <- renderPrint({
-    str(input$dynamic)
-  })
+  
+  
+  
+  
+  # output$graph <- renderPlot({
+  #   
+  #   #check to see what the graph is
+  #   if(input$selectData == "Bird Survey"){
+  #     if(input$graphType == "Histogram"){
+  #       
+  #       #Got stuff working and then it stopped working
+  #       #hist(bird_size$bird_size_species[,2])
+  #       #hist(birdSurvey)
+  #       
+  #       ggplot(locationData, aes(PredominantSpeciesType)) + geom_bar(fill=c("red", "blue")) + xlab("Predominant Species Type: Core or Transient") + ylab("Species Type Count")
+  #     
+  #     }else if(input$graphType == "Scatter Plot"){
+  #       ggplot(locationData, aes(NumSpecies,NDVI)) + geom_point(aes(colour = factor(PredominantSpeciesType), size = Elevation)) + xlab("Total Number of Species")
+  #         
+  #       
+  #     }else{
+  #       print("Graph unavailable for this dataset")
+  #         
+  #       }
+  #    
+  #     #If the dataset isn't bird survey than it should be dynamic
+  #     #bird survey data is hard coded
+  #   }else if(input$selectData != "Bird Survey"){
+  #     output$ui <- renderUI({
+  #       if(is.null(input$graphType))
+  #         return()
+  #       
+  #       switch(input$graphType,
+  #              "Histogram" = checkboxGroupInput(inputId = "histogramVariableOptions", label = "Select a variable to graph",
+  #                                               choices = c(10 = "option1", 344 = "option2", 567 = "option3")
+  #               ),
+  #              "Scatter Plot" = checkboxGroupInput(inputId = "scatplotVariableOptions", label = "Select two variables to graph",
+  #                                                  choices = c(10 = "option1", 344 = "option2", 567 = "option3")
+  #               ),
+  #              "Box-Whisker" = checkboxGroupInput(inputId = "boxVariableOptions1", label = "Select one below",
+  #                                                 choices = c(10 = "option1", 344 = "option2", 567 = "option3")
+  #               ) 
+  #              
+  #              )
+  #       
+  #     })
+  #     
+  #     #just trying to get a graph to show up if it's not bird survey
+  #     output$graph <- renderPlot({
+  #       hist(AirPassengers)
+  #       
+  #     })
+  #     
+  #     
+  #     
+  #   }
+  #   
+  #   
+  # })
   
 }
+)
+
+# function(input, output) {
+#   
+#   output$ui <- renderUI({
+#     if (is.null(input$input_type))
+#       return()
+#     
+#     # Depending on input$input_type, we'll generate a different
+#     # UI component and send it to the client.
+#     switch(input$input_type,
+#            "slider" = sliderInput("dynamic", "Dynamic",
+#                                   min = 1, max = 20, value = 10),
+#            "text" = textInput("dynamic", "Dynamic",
+#                               value = "starting value"),
+#            "numeric" =  numericInput("dynamic", "Dynamic",
+#                                      value = 12),
+#            "checkbox" = checkboxInput("dynamic", "Dynamic",
+#                                       value = TRUE),
+#            "checkboxGroup" = checkboxGroupInput("dynamic", "Dynamic",
+#                                                 choices = c("Option 1" = "option1",
+#                                                             "Option 2" = "option2"),
+#                                                 selected = "option2"
+#            ),
+#            "radioButtons" = radioButtons("dynamic", "Dynamic",
+#                                          choices = c("Option 1" = "option1",
+#                                                      "Option 2" = "option2"),
+#                                          selected = "option2"
+#            ),
+#            "selectInput" = selectInput("dynamic", "Dynamic",
+#                                        choices = c("Option 1" = "option1",
+#                                                    "Option 2" = "option2"),
+#                                        selected = "option2"
+#            ),
+#            "selectInput (multi)" = selectInput("dynamic", "Dynamic",
+#                                                choices = c("Option 1" = "option1",
+#                                                            "Option 2" = "option2"),
+#                                                selected = c("option1", "option2"),
+#                                                multiple = TRUE
+#            ),
+#            "date" = dateInput("dynamic", "Dynamic"),
+#            "daterange" = dateRangeInput("dynamic", "Dynamic")
+#     )
+#   })
+#   
+#   output$input_type_text <- renderText({
+#     input$input_type
+#   })
+#   
+#   output$dynamic_value <- renderPrint({
+#     str(input$dynamic)
+#   })
+#   
+# }
--- ui.R
+++ ui.R
@@ -8,55 +8,61 @@
 library(shiny)
 library(rdataretriever)
 
-# Define UI for application that draws a histogram
-#shinyUI(fluidPage(
-#  titlePanel("EcoData Viewer"),
-  
-#  sidebarLayout(
-#    sidebarPanel(
-#      helpText("Create informative graphs with datasets from the EcoData Retriever."),
-      
-#      selectInput(inputId = "selectData",
-#                  label="Choose the dataset to graph",
-#                  choices = c("Bird Survey", "Vertnet-Fishes")
-#                  ),
-      
-#      radioButtons(inputId = "graphType",
-#                   label = "Select the desired graph",
-#                   choices = c("Histogram", "Scatter Plot", "Box-Whisker")
-#                   )
-#    ),
-    
-    #mainpage goes here
-#    mainPanel(
-#      plotOutput("graph"))
-#  )
-#))
+#Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  titlePanel("EcoData Viewer"),
 
-fluidPage(
-  titlePanel("Dynamically generated user interface components"),
-  fluidRow(
-    
-    column(3, wellPanel(
-      selectInput("input_type", "Input type",
-                  c("slider", "text", "numeric", "checkbox",
-                    "checkboxGroup", "radioButtons", "selectInput",
-                    "selectInput (multi)", "date", "daterange"
-                  )
-      )
-    )),
-    
-    column(3, wellPanel(
-      # This outputs the dynamic UI component
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create informative graphs with datasets from the EcoData Retriever."),
+
+      selectInput(inputId = "selectData",
+                  label="Choose the dataset to graph",
+                  choices = c("Bird Survey", "Portal")
+                  ),
+
+      radioButtons(inputId = "graphType",
+                   label = "Select the desired graph",
+                   choices = c("Histogram", "Scatter Plot", "Box-Whisker")
+                   ),
+      
+      #pops up checkbox based on what the graphType is..right?
       uiOutput("ui")
-    )),
-    
-    column(3,
-           tags$p("Input type:"),
-           verbatimTextOutput("input_type_text"),
-           tags$p("Dynamic input value:"),
-           verbatimTextOutput("dynamic_value")
-    )
+      
+    ),
+
+    #mainpage goes here
+    mainPanel(
+      plotOutput("graph"))
   )
-)
+))
+
+
+#DYNAMIC EXAMPLE STUFF
+# fluidPage(
+#   titlePanel("Dynamically generated user interface components"),
+#   fluidRow(
+#     
+#     column(3, wellPanel(
+#       selectInput("input_type", "Input type",
+#                   c("slider", "text", "numeric", "checkbox",
+#                     "checkboxGroup", "radioButtons", "selectInput",
+#                     "selectInput (multi)", "date", "daterange"
+#                   )
+#       )
+#     )),
+#     
+#     column(3, wellPanel(
+#       # This outputs the dynamic UI component
+#       uiOutput("ui")
+#     )),
+#     
+#     column(3,
+#            tags$p("Input type:"),
+#            verbatimTextOutput("input_type_text"),
+#            tags$p("Dynamic input value:"),
+#            verbatimTextOutput("dynamic_value")
+#     )
+#   )
+# )
 
