1ba783662a6aea799b23faad360f6e4fe401b3c7
katythedress <gillespie.sunny@gmail.com>
2017-04-22 07:08:56 -0600

    Commenting out Iris information

--- ui.R
+++ ui.R
@@ -18,7 +18,7 @@ shinyUI(fluidPage(
 
       selectInput(inputId = "selectData",
                   label="Choose the dataset to graph",
-                  choices = c("Bird Survey", "Abalone Age Prediction", "Portal", "Salmon Trends", "Iris", "Antarctic Bird Species")
+                  choices = c("Bird Survey", "Abalone Age Prediction", "Portal", "Salmon Trends", "Antarctic Bird Species")
                   ),
 
       radioButtons(inputId = "graphType",
